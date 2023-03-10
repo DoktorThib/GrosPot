74c32033e115f7164ccd47a99d3d9a06444273c4
DoktorThib <Thibault.doktor@gmail.com>
2023-03-10 20:53:20 +0100

    Update VLX control - No test

--- main/GrosPot_VLX.cpp
+++ main/GrosPot_VLX.cpp
@@ -1,9 +1,9 @@
 #include "GrosPot_VLX.h"
 #include <Wire.h>
 
-#include <Adafruit_VL53L0X.h>
+#include <Adafruit_VL53L0X.h> /*TO REMOVE*/
+#include "VL53L0X.h"
 #include "GrosPot_DriverPwm.h"
-
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
@@ -12,11 +12,248 @@ void VLX_init(void){
   DRIVERPWM_setOnOff(PIN_PWM_SHUT_AvtG, false);
   DRIVERPWM_setOnOff(PIN_PWM_SHUT_AvtM, false);
   DRIVERPWM_setOnOff(PIN_PWM_SHUT_AvtD, false);
+
   DRIVERPWM_setOnOff(PIN_PWM_SHUT_ArrG, false);
   DRIVERPWM_setOnOff(PIN_PWM_SHUT_ArrM, false);
   DRIVERPWM_setOnOff(PIN_PWM_SHUT_ArrD, false);
 }
 
-void VLX_getDistCm(int pin, float* value){
+// VL15310x objects declaration
+tof_obj tof_front;
+tof_obj tof_back;
 
+float last_detection;
+
+/*Declare and init number of detection*/
+nbDetecTOF frontTOF_detect = {0,0,0};
+nbDetecTOF frontTOF_NOT_detect = {0,0,0};
+
+nbDetecTOF backTOF_detect = {0,0,0};
+nbDetecTOF backTOF_NOT_detect = {0,0,0};
+
+void set_ID(){
+
+  // all reset to LOW
+  set_ToF_High_Low(LOW);
+  delay(100);
+
+  // all unreset to HIGH
+  set_ToF_High_Low(HIGH);
+  delay(100);
+
+  // all reset to LOW
+  set_ToF_High_Low(LOW);
+
+  /*Set ID for each ToF*/
+  set_ID_error_handler(PIN_PWM_SHUT_AvtG,1, tof_front.right, LEFT_TOF_FRONT_ADDRESS);
+  set_ID_error_handler(PIN_PWM_SHUT_AvtM,2, tof_front.right, CENTER_TOF_FRONT_ADDRESS);
+  set_ID_error_handler(PIN_PWM_SHUT_AvtD,3, tof_front.right, RIGHT_TOF_FRONT_ADDRESS);
+
+  set_ID_error_handler(PIN_PWM_SHUT_ArrG,4, tof_back.right, LEFT_TOF_BACK_ADDRESS);
+  set_ID_error_handler(PIN_PWM_SHUT_ArrM,5, tof_back.right, CENTER_TOF_BACK_ADDRESS);
+  set_ID_error_handler(PIN_PWM_SHUT_ArrD,6, tof_back.right, RIGHT_TOF_BACK_ADDRESS);
+}
+
+/*argument
+    int pin_TOF --> Xshut pin
+    int tof_id --> number tof identificate it in Serial
+    VL53L0X tof_obj --> tof object correponding to front or back, left, center or right
+    int addr_TOF --> adress for I2C
+    */
+void set_ID_error_handler(int pin_TOF,int tof_id, VL53L0X tof_obj, int addr_TOF){
+  // activating RIGHT_TOF
+  digitalWrite(pin_TOF, HIGH);
+  delay(10);
+
+  //Serial com pin_TOF
+  Serial.print("Init ToF ");
+  Serial.println(tof_id);
+
+  //tof_right_front.setTimeout(500);
+  
+  while(!tof_obj.init(true)) {
+    set_led(RIGHT_LED, LOW);
+    Serial.println("Failed to init pin_TOF !");
+    delay(500);
+    set_led(RIGHT_LED, HIGH);
+  }
+  tof_obj.setAddress(addr_TOF);
+  delay(100);
+}
+
+void set_ToF_High_Low(int state){
+  DRIVERPWM_setOnOff(PIN_PWM_SHUT_AvtG, state);
+  DRIVERPWM_setOnOff(PIN_PWM_SHUT_AvtM, state);
+  DRIVERPWM_setOnOff(PIN_PWM_SHUT_AvtD, state);
+  
+  DRIVERPWM_setOnOff(PIN_PWM_SHUT_ArrG, state);
+  DRIVERPWM_setOnOff(PIN_PWM_SHUT_ArrM, state);
+  DRIVERPWM_setOnOff(PIN_PWM_SHUT_ArrD, state);
+}
+
+void TOF_init(){
+  Serial.println("Init ToF");
+  pinMode(LEFT_TOF_FRONT, OUTPUT);
+  pinMode(CENTER_TOF_FRONT, OUTPUT);
+  pinMode(RIGHT_TOF_FRONT, OUTPUT);
+
+  pinMode(LEFT_TOF_BACK, OUTPUT);
+  pinMode(CENTER_TOF_BACK, OUTPUT);
+  pinMode(RIGHT_TOF_BACK, OUTPUT);
+
+  /*Set all Tof To LOW*/
+  set_ToF_High_Low(LOW);//useless TO REMOVE set ID set to LOW at beginning
+  set_ID();
+  delay(10);
+
+/*Start continous acquisition startContinuous(uint32_t period_ms)*/
+/*period_ms == NULL, perma acquisition*/
+  tof_front.left.startContinuous();
+  delay(10);
+  tof_front.center.startContinuous();
+  delay(10);
+  tof_front.right.startContinuous();
+  delay(10);
+  tof_back.left.startContinuous();
+  delay(10);
+  tof_back.center.startContinuous();
+  delay(10);
+  tof_back.right.startContinuous();
+  delay(10);
+}
+
+/*argument
+    float* measure --> data read for distance
+    VL53L0X tof_obj --> tof object correponding to front or back, left, center or right
+    */
+bool get_measure(VL53L0X tof_obj, float* measure){
+  /*Gat distance read by tof_obj*/
+  measure = tof_obj.readRangeContinuousMillimeters();
+  /* return true is data is available OR false if data is not readable*/
+   return !(tof_obj.timeoutOccurred() || measure ==0);
+}
+
+/*argument
+struct contain all distance --> API
+dist_tof dist_TOF --> get all distance in struct*/
+/*TO DO err data*/
+bool Update_tof(dist_tof dist_TOF ){
+
+  float measure = 0.0;
+  bool err = false;
+
+  err = get_measure(tof_front.left, measure);
+  dist_TOF.front_left = measure;
+
+  err = get_measure(tof_front.center, measure);
+  dist_TOF.front_center = measure;
+
+  err = get_measure(tof_front.right, measure);
+  dist_TOF.front_right = measure;
+
+  err = get_measure(tof_back.left, measure);
+  dist_TOF.back_left = measure;
+
+  err = get_measure(tof_back.center, measure);
+  dist_TOF.back_center = measure;
+
+  err = get_measure(tof_back.right, measure);
+  dist_TOF.back_right = measure;   
+
+  return err;
+}
+
+/*argument
+  int front_back --> 0 front / 1 back*/
+boolean Is_bot(int front_back){
+  bool l = false, c = false, r = false;
+  bool detected = false;
+
+  /*IF looking in front of us*/
+  if(0 == front_back){
+    l = is_bot_direction(frontTOF_detect.left, frontTOF_NOT_detect.left);
+    c = is_bot_direction(frontTOF_detect.center, frontTOF_NOT_detect.center);
+    r = is_bot_direction(frontTOF_detect.right, frontTOF_NOT_detect.right);
+  }
+  /*IF we are looking behind us*/
+  else if (1 == front_back){
+    l = is_bot_direction(backTOF_detect.left, backTOF_NOT_detect.left);
+    c = is_bot_direction(backTOF_detect.center, backTOF_NOT_detect.center);
+    r = is_bot_direction(backTOF_detect.right, backTOF_NOT_detect.right);
+  }
+  else{
+    /*Add error ?*/
+  }
+
+  /*IF one detected something*/
+  if(l || c || r){
+    last_detection = millis();
+    detected = true;
+  }
+  else{
+    /*TO DO timer to verify between two values*/
+    if(millis() - last_detection < 1500){
+      detected = true;
+    }
+    else{
+      detected = false;
+    }
+  }
+  return detected;
+}
+
+/*Description : this function filter wrong data by reading multiple value in raw*/
+boolean is_bot_direction(int* nb_detect, int* nb_NOT_detect ){
+  float* measure = NULL;
+  bool distOK = false;
+  bool detected = false;
+  get_measure(tof_front.left,measure);
+
+  /**TO DO check value between 30 and 150, maybe problem with rouleau compresseur*/
+  /*TO DO check float in comparaison in "if"*/
+  /*IF distance is over 30mm OR between 150mm to 800mm AND dist OK*/
+  if((measure < 30 || (measure > 150 && measure < 800)) && false == distOK ){
+    /*Got a new detection*/
+    nb_detect ++;
+    /*IF nb detection is OVER required nb times to detect*/
+    if(nb_detect >= MIN_DETECT){
+      /*RESET value and prepared to return seing object*/
+      nb_NOT_detect = 0;
+      nb_detect = MIN_DETECT;
+      detected = true;
+    }
+    else{
+      detected = false;
+    }
+  }else{
+    nb_NOT_detect ++;
+    if(nb_NOT_detect >= MIN_NOT_DETECT){
+      nb_detect = 0;
+      nb_NOT_detect = MIN_NOT_DETECT;
+      detected = false;
+    }else{
+      detected = true;
+    }
+  }
+  return detected;
+}
+
+void Plot_tof(){
+  /*Receive dist from all ToF*/
+  dist_tof dist_TOF_lcl = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0,};
+  Update_tof(dist_TOF_lcl);
+
+  Serial.print("ToF_Left front: ");
+  Serial.println(dist_TOF_lcl.front_left);
+  Serial.print(", ToF_Center front: ");
+  Serial.println(dist_TOF_lcl.front_center);
+  Serial.print(", ToF_Right front: ");
+  Serial.println(dist_TOF_lcl.front_right);
+
+  Serial.print("ToF_Left back: ");
+  Serial.println(dist_TOF_lcl.back_left);
+  Serial.print(", ToF_Center back: ");
+  Serial.println(dist_TOF_lcl.back_center);
+  Serial.print(", ToF_Right back: ");
+  Serial.println(dist_TOF_lcl.back_right);
 }
--- main/GrosPot_VLX.h
+++ main/GrosPot_VLX.h
@@ -5,8 +5,55 @@
 
 #include "param.h"
 
-void VLX_init(void);
+#define LEFT_TOF_FRONT    //A3 XSHUT
+#define CENTER_TOF_FRONT  //A1 XSHUT
+#define RIGHT_TOF_FRONT   //A2 XSHUT
 
-void VLX_getDistCm(int pin, float* value);
+#define LEFT_TOF_BACK     //A3 XSHUT
+#define CENTER_TOF_BACK   //A1 XSHUT
+#define RIGHT_TOF_BACK    //A2 XSHUT
+
+#define LEFT_TOF_FRONT_ADDRESS   22
+#define CENTER_TOF_FRONT_ADDRESS 25
+#define RIGHT_TOF_FRONT_ADDRESS  28
+#define LEFT_TOF_BACK_ADDRESS    30
+#define CENTER_TOF_BACK_ADDRESS  32
+#define RIGHT_TOF_BACK_ADDRESS   34
+
+// VL15310x object 
+struct tof_obj{
+  VL53L0X left;
+  VL53L0X center;
+  VL53L0X right;
+};
+
+//number of detection
+struct nbDetecTOF{
+  int left;
+  int center;
+  int right;
+};
+
+//Update struct ToF
+struct dist_tof{
+  float front_left;
+  float front_center;
+  float front_right;
+  float back_left;
+  float back_center;
+  float back_right;
+};
+
+/*API*/
+void TOF_init();
+bool Update_tof(dist_tof dist_TOF );
+boolean Is_bot(int front_back);
+void Plot_tof();
+
+/*Local function*/
+void set_ID_error_handler(int pin_TOF,int tof_id, VL53L0X tof_obj, int addr_TOF);
+void set_ToF_High_Low(int state);
+bool get_measure(VL53L0X tof_obj, float* measure);
+boolean is_bot_direction(int* nb_detect, int* nb_NOT_detect );
 
 #endif
